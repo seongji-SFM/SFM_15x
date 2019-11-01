d0c915b3528670c296a91304b26964b5d721765d
yepark-wisol <yepark@wisol.co.kr>
2019-11-01 10:42:28 +0900

    error fix : write wifi rf test bin

--- development/SFM/tools/wifitools/Certification_Test_write_CE_Adaptivity.cmd
+++ development/SFM/tools/wifitools/Certification_Test_write_CE_Adaptivity.cmd
@@ -2,7 +2,7 @@
 
 set UART_PORT=COM64
 
-esptool --port %UART_PORT% -b 460800 write_flash --flash_mode dout --flash_freq 80m --flash_size 1MB --verify 0x0000 WIFI_Certification_ESP_RF_test_tool_v2.0/bin/ESP8266_RF_TEST_BIN_V130_26m_20190731.bin
+esptool --port %UART_PORT% -b 460800 write_flash --flash_mode dout --flash_freq 80m --flash_size 1MB --verify 0x0000 WIFI_Certification_ESP_RF_test_tool_v2.0/bin/ESP8266_Adaptivity_v2.1_26m_20170726_1M.bin
 if not "%ERRORLEVEL%" == "0" (
 echo WIFI Write Error
 pause
